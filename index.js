66e9f34ee4fa2568f843ef11b5f724119ff09915
Magen Crawford <crawford.magen@gmail.com>
2020-11-15 21:40:03 -0600

    updated design and researched js for carousel and expand

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-console.log("It Works");
+
+//this is copy and pasted code, 
+//more of a guidline, 
+//pay no attention to that man behind the curtain
+
+// const mediaQuery = window.matchMedia('(max-width: 700px)')
+
+// function handleTabletChange(e) {
+//     // Check if the media query is true
+//     if (e.matches) {
+//         // Then log the following message to the console
+//         console.log('Media Query Matched!')
+//     }
+// }
+
+// // Register event listener
+// mediaQuery.addListener(handleTabletChange)
+
+// // Initial check
+// handleTabletChange(mediaQuery)
+
+
+// var slideIndex = 1;
+// showSlides(slideIndex);
+
+// function plusSlides(n) {
+// showSlides((slideIndex += n));
+// }
+
+// function currentSlide(n) {
+// showSlides((slideIndex = n));
+// }
+
+// function showSlides(n) {
+// var i;
+// var slides = document.getElementsByClassName("item");
+// if (n > slides.length) {
+//   slideIndex = 1;
+// }
+// if (n < 1) {
+//   slideIndex = slides.length;
+// }
+// for (i = 0; i < slides.length; i++) {
+//   slides[i].style.display = "none";
+// }
+
+// slides[slideIndex - 1].style.display = "block";
+// }
